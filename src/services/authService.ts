acd95e38141d3077c011d7986b67876fb75f8b1d
Noan Brostt <calielnoan@gmail.com>
2025-07-03 15:12:40 -0300

    console log api key

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_LOGIN_API_KEY;
 
-export const login = (matricula: string, senha: string) =>
+export const login = (matricula: string, senha: string) => {
+  console.log(API_KEY);
   axios.post("/auth/login", { matricula, senha, api_key: API_KEY });
+}
 
 export const reset = (matricula: string, cpf: string, nova_senha: string) =>
   axios.post("/auth/resetar_senha", { matricula, cpf, nova_senha, api_key: API_KEY });
