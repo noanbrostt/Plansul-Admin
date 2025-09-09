897970285a2ee0a5c6326187b933e575b47e85c8
Noan Brostt <calielnoan@gmail.com>
2025-09-09 15:34:55 -0300

    Dinâmizando react-router com gh-pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 import ProtectedRoute from "./ProtectedRoute";
@@ -49,7 +49,7 @@ const CompostosPage = lazy(() => import("@/modules/devs/graficos/CompostosPage.j
 // Devs - Playground
 const TypingGame = lazy(() => import("@/modules/devs/playground/TypingGame"));
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   // Login
   {
     path: "/login",
